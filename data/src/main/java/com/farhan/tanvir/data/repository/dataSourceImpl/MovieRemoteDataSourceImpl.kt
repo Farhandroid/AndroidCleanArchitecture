e10560667d1921878a340929563d59305c2f711c
Farhan Tanvir <farhantanvir65@gmail.com>
2022-02-21 15:26:16 +0900

    modify gitignore file

--- data/src/main/java/com/farhan/tanvir/data/repository/dataSourceImpl/MovieRemoteDataSourceImpl.kt
+++ data/src/main/java/com/farhan/tanvir/data/repository/dataSourceImpl/MovieRemoteDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.farhan.tanvir.data.repository.dataSourceImpl
 
-import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
